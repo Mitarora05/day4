a98b225529e9062793a872163703afbdc1231202
mitarora <mitarora@deloitte.com>
2023-04-12 15:25:22 +0530

    passing props through components using React.FC

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,12 +1,17 @@
 import Todos from "../../models/todo";
+import TodoItem from "../TodoItem/TodoItem";
 
 interface ITodo {
-    items: Todos[];
+  items: Todos[];
 }
-const Todo: React.FC<ITodo> = ({items}) => {
-    return <div>{items.map((item) => (
-        <li key={item.id}>{item.text}</li>
-    ) )}</div>  
-}
+const Todo: React.FC<ITodo> = ({ items }) => {
+  return (
+    <div>
+      {items.map((item) => (
+        <TodoItem key={item.id} text={item.text}/>
+      ))}
+    </div>
+  );
+};
 
 export default Todo;
